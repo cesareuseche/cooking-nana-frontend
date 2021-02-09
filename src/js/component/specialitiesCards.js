5a5f26426205ec133e404029d2ffbc6b034ae64a
humbi-exe <humberto.becerra95@gmail.com>
2021-02-09 18:59:49 -0400

    nuevos iconos specialities

--- src/js/component/specialitiesCards.js
+++ src/js/component/specialitiesCards.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "../../styles/SpecialitiesCards.css";
+import Sushi from "../../img/sushi.png";
+import Pizza from "../../img/pizza.png";
+import Taco from "../../img/taco.png";
+import Usa from "../../img/USA.png";
 
 function SpecialitiesCards() {
 	return (
@@ -8,27 +12,27 @@ function SpecialitiesCards() {
 				<div className="container-fluid presentation-width">
 					<div className="row">
 						<div className="col-md-3 specialities-align">
-							<div className="box">
-								<img src="https://img.icons8.com/color/96/000000/sushi.png" />
-								<h6>Asian</h6>
+							<div className="box sushi">
+								<img src={Sushi} alt="icon" />
+								<h6>ASIAN</h6>
 							</div>
 						</div>
 						<div className="col-md-3 specialities-align">
-							<div className="box">
-								<img src="https://img.icons8.com/color/96/000000/pizza.png" />
-								<h6>Italian</h6>
+							<div className="box pizza">
+								<img src={Pizza} />
+								<h6>ITALIAN</h6>
 							</div>
 						</div>
 						<div className="col-md-3 specialities-align">
-							<div className="box">
-								<img src="https://img.icons8.com/color/96/000000/hamburger.png" />
-								<h6>American</h6>
+							<div className="box usa">
+								<img src={Usa} />
+								<h6>AMERICAN</h6>
 							</div>
 						</div>
 						<div className="col-md-3 specialities-align">
-							<div className="box">
-								<img src="https://img.icons8.com/color/96/000000/kawaii-taco.png" />
-								<h6>Mexican</h6>
+							<div className="box taco">
+								<img src={Taco} />
+								<h6>MEXICAN</h6>
 							</div>
 						</div>
 					</div>
