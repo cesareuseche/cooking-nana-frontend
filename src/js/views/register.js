d6d635760f17bfe57e460265893bd0e7642cc88a
Vincent <vinsh15@gmail.com>
2021-02-09 17:41:25 -0400

    all rdy

--- src/js/views/register.js
+++ src/js/views/register.js
@@ -6,7 +6,54 @@ import { Link } from "react-router-dom";
 function Register() {
 	return (
 		<React.Fragment>
-			<div />
+			<div className="all row">
+				<div className="left-cover col-md-5" />
+				<div className="principal col-md-7 d-flex flex-column align-items-center justify-content-center">
+					<Link to="/">
+						<img className="logo" src={logo} alt="logo" />
+					</Link>
+					<h1>Create your account</h1>
+					<form>
+						<input className="input" type="text" placeholder="Name..." id="user" name="user" size="25" />
+						<input
+							className="input"
+							type="text"
+							placeholder="Last name..."
+							id="password"
+							name="password"
+							size="25"
+						/>
+						<input
+							className="input"
+							type="email"
+							placeholder="Email..."
+							id="email"
+							name="email"
+							size="25"
+						/>
+						<p className="example-text">e.g. myusername@example.com</p>
+						<input
+							className="input"
+							type="password"
+							placeholder="Password..."
+							id="password"
+							name="password"
+							size="25"
+						/>
+						<input
+							className="input"
+							type="password"
+							placeholder="Repeat your password..."
+							id="password1"
+							name="password1"
+							size="25"
+						/>
+					</form>
+					<Link to="/register">
+						<button className="cta-select">Create an account</button>
+					</Link>
+				</div>
+			</div>
 		</React.Fragment>
 	);
 }
