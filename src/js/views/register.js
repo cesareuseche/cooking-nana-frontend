f8bf954a5841eb805f0730fa51f4e9ce72501de7
humbi-exe <humberto.becerra95@gmail.com>
2021-02-10 19:18:46 -0400

    register in process

--- src/js/views/register.js
+++ src/js/views/register.js
@@ -1,57 +1,27 @@
 import React from "react";
-import "../../styles/Login.css";
+import "../../styles/Register.css";
 import logo from "../../img/logo.png";
 import { Link } from "react-router-dom";
 
 function Register() {
 	return (
-		<React.Fragment>
-			<div className="all row">
-				<div className="left-cover col-md-5" />
-				<div className="principal col-md-7 d-flex flex-column align-items-center justify-content-center">
-					<h1>Create your account</h1>
-					<form>
-						<input className="input" type="text" placeholder="Name..." id="user" name="user" size="25" />
-						<input
-							className="input"
-							type="text"
-							placeholder="Last name..."
-							id="password"
-							name="password"
-							size="25"
-						/>
-						<input
-							className="input"
-							type="email"
-							placeholder="Email..."
-							id="email"
-							name="email"
-							size="25"
-						/>
-						<p className="example-text">e.g. myusername@example.com</p>
-						<input
-							className="input"
-							type="password"
-							placeholder="Password..."
-							id="password"
-							name="password"
-							size="25"
-						/>
-						<input
-							className="input"
-							type="password"
-							placeholder="Repeat your password..."
-							id="password1"
-							name="password1"
-							size="25"
-						/>
-					</form>
-					<Link to="/">
-						<button className="cta-select">Create an account</button>
-					</Link>
+		<div className="login">
+			<div className="login__container">
+				<img src={logo} alt="" />
+				<h1>Welcome to Cooking Nana</h1>
+				<div className="register-inputs-alignment">
+					<input className="register-input" type="text" placeholder="Name" />
+					<input className="register-input" type="password" placeholder="Last Name" />
+					<input className="register-input" type="text" placeholder="Username" />
+					<input className="register-input" type="password" placeholder="Email" />
+					<input className="register-input" type="password" placeholder="Password" />
+					<input className="register-input" type="password" placeholder="Reset Password" />
+				</div>
+				<div className="register-buttons">
+					<button className="cta-select"> Create an Account </button>
 				</div>
 			</div>
-		</React.Fragment>
+		</div>
 	);
 }
 
