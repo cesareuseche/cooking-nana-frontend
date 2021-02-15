f6fa263802c436be7d0ad11770b856476e5baec1
cesareuseche <cesareuseche@gmail.com>
2021-02-15 18:33:52 -0400

    finished the ingriedients view

--- src/js/views/ingredientSelection.js
+++ src/js/views/ingredientSelection.js
@@ -1,9 +1,107 @@
 import React from "react";
+import "../../styles/IngredientSelection.css";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import protein from "../../img/protein.png";
+import veggie from "../../img/veggie.png";
+import carbs from "../../img/carbs.png";
+import { Link } from "react-router-dom";
 
 function IngredientSelection() {
 	return (
 		<React.Fragment>
-			<h1>Hola!</h1>
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12 ingredients-selection">
+						<h1>Choose your ingredients</h1>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-4 ingredients-icons">
+						<img src={protein} alt="protein" />
+						<div className="drowdown space-drodown">
+							<button
+								className="btn btn-secondary btn-protein dropdown-toggle"
+								type="button"
+								id="dropdownMenuButton"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false">
+								Select your Protein
+							</button>
+							<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+								<a className="dropdown-item" href="#">
+									Action
+								</a>
+								<a className="dropdown-item" href="#">
+									Another action
+								</a>
+								<a className="dropdown-item" href="#">
+									Something else here
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-4 ingredients-icons">
+						<img src={carbs} alt="protein" />
+						<div className="drowdown space-drodown">
+							<button
+								className="btn btn-secondary btn-carb dropdown-toggle"
+								type="button"
+								id="dropdownMenuButton"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false">
+								Select your Carb
+							</button>
+							<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+								<a className="dropdown-item" href="#">
+									Action
+								</a>
+								<a className="dropdown-item" href="#">
+									Another action
+								</a>
+								<a className="dropdown-item" href="#">
+									Something else here
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-4 ingredients-icons">
+						<img src={veggie} alt="protein" />
+						<div className="drowdown space-drodown">
+							<button
+								className="btn btn-secondary btn-veggie dropdown-toggle "
+								type="button"
+								id="dropdownMenuButton"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false">
+								Select your Veggie
+							</button>
+							<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+								<a className="dropdown-item" href="#">
+									Action
+								</a>
+								<a className="dropdown-item" href="#">
+									Another action
+								</a>
+								<a className="dropdown-item" href="#">
+									Something else here
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-12 button-alignment button-style">
+						<Link to="/recipes">
+							<button>Search your Recipe</button>
+						</Link>
+					</div>
+				</div>
+			</div>
 		</React.Fragment>
 	);
 }
