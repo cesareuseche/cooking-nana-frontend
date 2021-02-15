cfaf035a816896b7defbdebbda01a1b9a8cffc9a
Vincent Perez <vinsh15@gmail.com>
2021-02-15 18:41:39 -0400

    Revert "regirter fetch terminado"

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -4,12 +4,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {},
 		actions: {
-			registerContact: async (email, name, last_name, username, password) => {
+			registerContact: async ({ nombre, last_name, username, email, password }) => {
 				let url = BASE_URL + "/register";
-				actions = getActions();
 				let new_user = {
 					email: email,
-					name: name,
+					name: nombre,
 					last_name: last_name,
 					username: username,
 					password: password
@@ -17,38 +16,18 @@ const getState = ({ getStore, getActions, setStore }) => {
 				console.log(JSON.stringify(new_user));
 				let response = await fetch(url, {
 					method: "POST",
-					headers: { "Content-Type": "application/json" },
+					headers: { "Content-Type": "application-json" },
 					body: JSON.stringify(new_user)
 				});
 				if (response.ok) {
-					actions.login(new_user.username, new_user.password);
+					let created_user = JSON.stringify(response.body);
+					console.log(created_user);
 					return true;
 				} else {
-					console.log(response.statusText);
-					console.log(response.status);
+					console.log(JSON.stringify(response.body));
+					console.log(JSON.stringify(response.status));
 					return false;
 				}
-			},
-
-			login: async (username = null, email = null, password) => {
-				let url = BASE_URL + "/login";
-				let login_data = {};
-				if (username != null) {
-					login_data = {
-						username: username,
-						password: password
-					};
-				} else {
-					login_data = {
-						email: username,
-						password: password
-					};
-				}
-				let response = await fetch(url, {
-					method: "POST",
-					headers: { "Content-Type": "application/json" },
-					body: JSON.stringify(login_data)
-				});
 			}
 		}
 	};
