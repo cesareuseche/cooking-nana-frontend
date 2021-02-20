ed8cc9221b32d83365595e3961d62ff85d220156
Vincent <vinsh15@gmail.com>
2021-02-20 14:26:35 -0400

    aaaaa

--- src/js/views/recipes.js
+++ src/js/views/recipes.js
@@ -6,38 +6,38 @@ import PropTypes from "prop-types";
 
 function Recipes(selectedIngredients) {
 	const { store, actions } = useContext(Context);
-	let recipes = [];
-	useEffect(() => {
-		let ingredients = [];
-		let matchs = [];
-		let counter = 0;
-		let counter1 = 0;
-		let match = 0;
-		for (let i = 0; i < store.recipes.length; i++) {
-			ingredients = store.recipes[i].ingredients;
-			for (let j = 0; j < selectedIngredients.length; j++) {
-				for (let k = 0; k < ingredients.length; k++) {
-					if (selectedIngredients[j] == ingredients[k]) {
-						counter++;
-						break;
-					}
-				}
-			}
-			counter1 = store.recipes[i].ingredients.length;
-			match = (counter * 100) / counter1;
-			if (match >= 70) {
-				matchs.push(match);
-				recipes.push(store.recipes[i]);
-			}
-		}
-	});
+	// let recipes = [];
+	// useEffect(() => {
+	// 	let ingredients = [];
+	// 	let matchs = [];
+	// 	let counter = 0;
+	// 	let counter1 = 0;
+	// 	let match = 0;
+	// 	for (let i = 0; i < store.recipes.length; i++) {
+	// 		ingredients = store.recipes[i].ingredients;
+	// 		for (let j = 0; j < selectedIngredients.length; j++) {
+	// 			for (let k = 0; k < ingredients.length; k++) {
+	// 				if (selectedIngredients[j] == ingredients[k]) {
+	// 					counter++;
+	// 					break;
+	// 				}
+	// 			}
+	// 		}
+	// 		counter1 = store.recipes[i].ingredients.length;
+	// 		match = (counter * 100) / counter1;
+	// 		if (match >= 70) {
+	// 			matchs.push(match);
+	// 			recipes.push(store.recipes[i]);
+	// 		}
+	// 	}
+	// });
 
 	return (
 		<React.Fragment>
 			<div className="container-fluid presentation-width recipe-result">
 				<div className="row mobile-margin first-result-align" />
 				<div className="recipe-row col-md-12">
-					{recipes.map((recipe, i) => (
+					{/* {recipes.map((recipe, i) => (
 						<RecipesResult
 							key={i}
 							img={recipe.img_url}
@@ -46,7 +46,7 @@ function Recipes(selectedIngredients) {
 							id={recipe.id}
 							match={matchs[i]}
 						/>
-					))}
+					))} */}
 				</div>
 			</div>
 		</React.Fragment>
