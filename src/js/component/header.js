c18a43d04d3070245f65d2b8f04399d13fc2b958
Vincent <vinsh15@gmail.com>
2021-02-20 14:37:54 -0400

    ultimo commit para cesar

--- src/js/component/header.js
+++ src/js/component/header.js
@@ -8,6 +8,23 @@ import { useStateValue } from "../store/stateProvider";
 
 export const Header = () => {
 	const [{ cart }, dispatch] = useStateValue();
+	// const logueado = false;	
+	// const { store, actions } = useContext(Context);	
+
+	// const checking = async () => {	
+	// 	const succes = await actions.check();	
+	// 	if (succes) {	
+	// 		logueado = true;	
+	// 		console.log(`el login esta en ${logueado}`);	
+	// 		console.log(`${localStorage.getItem("name")} estas logueado`);	
+	// 	} else {	
+	// 		console.log(`el login esta en ${logueado}`);	
+	// 	}	
+	// };	
+
+	// useEffect(() => {	
+	// 	checking();	
+	// }, []);
 
 	return (
 		<div className="header_area">
