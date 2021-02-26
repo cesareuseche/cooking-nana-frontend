0bb1b5fb925261aa3cb57bf2df07fec8663e4c7f
cesareuseche <cesareuseche@gmail.com>
2021-02-25 23:43:44 -0400

    typo fix

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -50,7 +50,7 @@ function Home() {
 			<div className="container-fluid presentation-width">
 				<div className="row">
 					<div className="col-md-12">
-						<h2>Recepies</h2>
+						<h2>Recepes</h2>
 					</div>
 				</div>
 			</div>
