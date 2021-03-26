7cfc1d207913ba634f0f87e464d5e0eedacfa2a5
cesareuseche <cesareuseche@gmail.com>
2021-03-26 16:32:42 -0400

    working on a new navbar

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 //import your own components
 import Layout from "./layout";
